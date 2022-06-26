9a7b0917b353742a8a538958a104bc7fcdd879f7
Oniyide Faith <72167184+oniyidefaith@users.noreply.github.com>
2022-06-26 23:11:14 +0100

    django crud

--- I4G01560653K/blog/views.py
+++ I4G01560653K/blog/views.py
@@ -9,17 +9,21 @@ from django.urls import reverse_lazy
 
 class PostListView(ListView):
     model = Post
-    def PostCreateView(CreateView):
-        model = Post,
-        fields = "__all__"
-        success_url  = reverse_lazy('blog:all')
-    def  PostDetailView(DetailView):
-        model = Post,
-    def PostUpdateView(UpdateView):
-        model = Post 
-        fields = "__all__" 
-        success_url  = reverse_lazy('blog:all')
-    def PostDeleteView(UpdateView):
-        model = Post 
-        fields = "__all__" 
-        success_url  = reverse_lazy('blog:all')
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+
+class PostDeleteView(UpdateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
